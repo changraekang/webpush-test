55b8d988d0a017785a92aea3b8020572b0b34724
changraekang <storkcr@gmail.com>
2023-01-04 14:30:43 +0900

    axios 주소 수정

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instanceAxios = axios.create({
-  // baseURL: "http://52.78.170.177:8080/api/",
-  baseURL: "http://localhost:8080/api/", // 로컬
+  baseURL: "https://52.78.170.177:8080/api/",
+  //baseURL: "http://localhost:8080/api/", // 로컬
   // headers : {
   //   Authorization : `${userAccessToken}`
   // }
