51e5e38515bef85855ba3aff2e9ba62f3c6bcb5d
kangchangrae <storkcr@gmail.com>
2023-01-17 16:04:03 +0900

    logout error handle

--- src/cookie/controlCookie.jsx
+++ src/cookie/controlCookie.jsx
@@ -52,17 +52,24 @@ export const logoutSession = async () => {
   window.localStorage.removeItem("recoil-persist");
   try {
     const response = await instanceAxios.post(`/member/logout`, logoutData);
+    const cookies = new Cookies();
     cookies.remove({ path: "/" });
     console.log(response);
     console.log("로그아웃");
     if (response.status === 200) {
+      const cookies = new Cookies();
       // dispatch({type: 'logout'})
+      cookies.remove("refreshToken");
+      cookies.remove("accessToken");
       instanceAxios.defaults.headers.common["Authorization"] = null;
       window.location.reload();
       console.log("로그아웃 성공🎉");
     }
   } catch (err) {
+    const cookies = new Cookies();
     cookies.remove({ path: "/" });
+    cookies.remove("refreshToken");
+    cookies.remove("accessToken");
     instanceAxios.defaults.headers.common["Authorization"] = null;
     window.location.reload();
     console.log("로그아웃 세션만료🎉");
